0eee28d4a6fbbbab5a88deca93eabc033d1f5c95
Sujal Pawar <sujalpawar00007@gmail.com>
2025-09-09 15:12:53 +0530

    feat: Simplify ChatbotButton to always display the agri leaf icon

--- client/src/components/common/ChatbotButton.jsx
+++ client/src/components/common/ChatbotButton.jsx
@@ -75,25 +75,8 @@ const ChatbotButton = () => {
       >
         <div className="absolute inset-0 bg-gradient-to-r from-green-400 to-emerald-500 rounded-full blur-md opacity-70"></div>
         <div className="relative z-10 flex items-center justify-center">
-          {!isButtonPulsing ? (
-            <>
-              {/* Normal state - farmer with crop icon */}
-              <div className="relative">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" viewBox="0 0 24 24" fill="currentColor">
-                  <path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8 0-1.846.634-3.542 1.688-4.897l11.209 11.209A7.946 7.946 0 0112 20zm6.312-3.103L7.103 5.688A7.948 7.948 0 0112 4c4.411 0 8 3.589 8 8a7.954 7.954 0 01-1.688 4.897z" />
-                  <path d="M16.5 8a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
-                </svg>
-                <span className="absolute -top-1.5 -right-1.5 bg-white rounded-full p-1 shadow-md">
-                  <FontAwesomeIcon icon={faWheatAwn} className="text-green-600 text-[10px]" />
-                </span>
-              </div>
-            </>
-          ) : (
-            <>
-              {/* Pulsing state - shows the agri leaf icon */}
-              <FontAwesomeIcon icon={faLeaf} className="text-white text-xl" />
-            </>
-          )}
+          {/* Always show the leaf icon - permanent state */}
+          <FontAwesomeIcon icon={faLeaf} className="text-white text-xl" />
         </div>
       </button>
     </div>
